eb7b9714f80f53c128b41a25a3901b4972540a24
liwanlin41 <liwanlin@mit.edu>
2019-04-15 23:53:03 -0400

    some bad display stuff

--- BeatRevolution/Display.cpp
+++ BeatRevolution/Display.cpp
@@ -1,10 +1,146 @@
 #include "Arduino.h"
 #include "Display.h"
 
-Display::Display(/* parameters such as various pins */){
-  
+Display::Display(TFT_eSPI* tft, float rate){
+  ppm = rate;
+  //initialize map
+  char_to_int['l'] = 0;
+  char_to_int['d'] = 1;
+  char_to_int['u'] = 2;
+  char_to_int['r'] = 3;
+  int_to_char[0] = 'l';
+  int_to_char[1] = 'd';
+  int_to_char[2] = 'u';
+  int_to_char[3] = 'r';
+  ind = 0;
+  screen = tft;
+  screen->fillScreen(BACKGROUND);
+
+  // for testing, change later
+  for(int i = 0; i<10; i++)
+  {
+    cur_dir[i] = i % 4;
+    cur_times[i] = 1000*(i+1);
+    past_ycoors[i] = -100;
+  }
+}
+
+// start song/timer
+void Display::start() {
+  start_time = millis();
 }
 
 void Display::process(/* parameters tbd */) {
-  
+  // use constant current time for all arrows
+  long cur_timer = millis();
+  //blank the previous arrows
+  for(int i = 0; i<10; i++)
+  {
+    if(past_ycoors[i]>0)
+    {
+      draw_arrow(int_to_char[cur_dir[i]],arr_x[cur_dir[i]],past_ycoors[i],BACKGROUND);
+    }
+    //new y coordinate of arrow
+    int y_coor = calc_center(cur_dir[i],cur_times[i],cur_timer);
+    if(y_coor > -2)
+    {
+      past_ycoors[i] = y_coor;
+    } else {
+      // will be to take the next note
+      past_ycoors[i] = -100;
+    }
+  }
+  Serial.println("new y values");
+  for(int i = 0; i<10; i++)
+  {
+    Serial.println(past_ycoors[i]);
+  }
+  // arrows at top of screen
+  for(int i = 0; i<4; i++)
+  {
+    draw_arrow(int_to_char[i],arr_x[i],arr_y[i],TFT_WHITE);
+  }
+  //draw arrows in new locations
+  for(int i = 0; i<10; i++)
+  {
+    if(past_ycoors[i] > 0)
+    {
+      draw_arrow(int_to_char[cur_dir[i]],arr_x[cur_dir[i]],past_ycoors[i],find_color(cur_times[i]));
+    }
+  }
+}
+
+// given time of beat and direction, find current y-center of arrow
+int Display::calc_center(int dir, long beat, long timer) {
+  long time_until = beat-timer+start_time;
+  Serial.println(time_until);
+  int disp = int(time_until*ppm);
+  if(disp > 132)
+  {
+    return -1; // too far away to display on screen
+  } else if (time_until < -1*thresh) // too far in other direction
+  {
+    return -2;
+  }
+  return (arr_y[dir] + disp);
+}
+
+// draw arrow on screen with direction dir, rectangle center (x,y)
+void Display::draw_arrow(char dir, int x, int y,uint16_t color){
+  // triangle points
+  int x0;
+  int x1;
+  int x2;
+  int y0;
+  int y1;
+  int y2;
+  switch(dir) {
+    case 'u':
+    {
+      x0 = x-6;
+      x1 = x+6;
+      x2 = x;
+      y0 = y-4;
+      y1 = y-4;
+      y2 = y-10;
+    }
+    break;
+    case 'd':
+    {
+      x0 = x-6;
+      x1 = x+6;
+      x2 = x;
+      y0 = y+4;
+      y1 = y+4;
+      y2 = y+10;
+    }
+    break;
+    case 'l':
+    {
+      x0 = x-4;
+      x1 = x-4;
+      x2 = x-10;
+      y0 = y-6;
+      y1 = y+6;
+      y2 = y;
+    }
+    break;
+    case 'r':
+    {
+      x0 = x+4;
+      x1 = x+4;
+      x2 = x+10;
+      y0 = y-6;
+      y1 = y+6;
+      y2 = y;
+    }
+    break;
+  }
+  screen->fillRect(x-3,y-3,7,7,color);
+  screen->fillTriangle(x0,y0,x1,y1,x2,y2,color);
+}
+
+// given the timing of a beat, determine the color of the arrow
+uint16_t Display::find_color(long beat) {
+  return TFT_GREEN;
 }
--- BeatRevolution/Display.h
+++ BeatRevolution/Display.h
@@ -1,6 +1,9 @@
 #ifndef Display_h
 #define Display_h
 #include "Arduino.h"
+#include <TFT_eSPI.h> 
+#include <SPI.h>
+#include <map>
 
 class Display
 {
@@ -8,6 +11,30 @@ class Display
 
   long* note_times;
   char* note_dirs;
+  TFT_eSPI* screen;
+  float ppm; // pixels per ms
+  const int thresh = 50; // maximum ms tolerance in the late direction
+  // convert char and int versions of direction
+  std::map<char, int> char_to_int;
+  std::map<int, char> int_to_char;
+  // coordinates for arrows at top of screen indicating correct hit time
+  const int arr_x[4] = {23,48,78,103};
+  const int arr_y[4] = {20,18,22,20};
+  
+  // background color
+  const uint16_t BACKGROUND = TFT_BLACK;
+
+  //past locations of current 10 arrows to be displayed
+  int past_ycoors[10];
+  // times for current arrows to be displayed
+  long cur_times[10];
+  // directions for current arrows to be displayed
+  int cur_dir[10];
+  //next insertion into list of 10
+  int ind;
+  // index of time and direction of next note to be added
+  int time_ind;
+  int dir_ind;
 
   uint32_t start_time;
 
@@ -17,9 +44,13 @@ class Display
 
   public:
   
-  Display(/* parameters such as various pins */);
+  Display(TFT_eSPI* tft, float rate); //rate is pixels per ms
+  void start(); // start timer
   void process(/* parameters tbd */);
   // TODO: other display methods as needed
+  void draw_arrow(char dir, int x, int y,uint16_t color);
+  int calc_center(int dir, long beat, long timer);
+  uint16_t find_color(long beat);
 };
 
 #endif

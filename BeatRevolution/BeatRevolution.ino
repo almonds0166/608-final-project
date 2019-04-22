92351058312b402274fa614c47341a32bdbb9176
almonds <almonds@users.noreply.github.mit.edu>
2019-04-22 16:20:35 -0400

    Update BeatRevolution.ino
    * Now connects to WiFi
    * Now downloads the chart `bad_apple.txt` into the appropriate variables (`note_dirs`, `note_times`, ...)
    * `display_left` and `display_right` were shunted into `displays[2]`
    * `saber_left` and `saber_right` were shunted into `sabers[2]`

--- BeatRevolution/BeatRevolution.ino
+++ BeatRevolution/BeatRevolution.ino
@@ -1,4 +1,14 @@
 #include "Game.h"
+#include <WiFi.h>
+
+// Loop speed
+#define FPS 30
+const float LOOP = 1000.0 / FPS;
+uint32_t primary_timer;
+
+// WiFi
+char NETWORK[] = "MIT"; // ssid
+char PW[]      = "";    // p/w
 
 TFT_eSPI tft = TFT_eSPI(); 
 MPU9255 imu;
@@ -8,16 +18,38 @@ const int cs_pin_right = 0;
 float rate = 0.05;
 
 Saber saber_left(&imu);
+Saber saber_right(&imu);
+
 Display display_left(&tft, rate, cs_pin_left);
 Display display_right(&tft, rate, cs_pin_right);
-Game game(&saber_left, &display_left, &display_right);
 
-char song_name[50] = "test";
+Saber*   sabers[2]   = {&saber_left, &saber_right};
+Display* displays[2] = {&display_left, &display_right};
+
+Game game(sabers, displays);
+
+char song_name[50] = "bad_apple"; // will eventually be an index
 boolean complete = false;
 
 void setup(void) {
   Serial.begin(115200);
-  // TODO: connect to wifi
+  while (!Serial); // wait until we can communicate
+
+  // Connect to WiFi
+  WiFi.begin(NETWORK, PW);
+  uint8_t connect_attempts = 0;
+  Serial.printf("Attempting to connect to\n%s WiFi ", NETWORK);
+  while (WiFi.status() != WL_CONNECTED && connect_attempts++ < 8) {
+    delay(500);
+    Serial.print('.');
+  }
+  delay(500);
+  if(!WiFi.isConnected()) {
+    Serial.println(" failed!\nRestarting in a bit...");
+    delay(5000);
+    ESP.restart();
+  }
+  Serial.println(" CONNECTED!");
 
   // NOTE: taking out the setupIMU stuff breaks everything
   
@@ -29,10 +61,10 @@ void setup(void) {
     ESP.restart(); // restart the ESP (proper way)
   }
   
-  
-
   game.load(song_name);
   game.start(song_name);
+  
+  primary_timer = millis();
 }
 
 void loop() {
@@ -43,4 +75,12 @@ void loop() {
     complete = game.process(); // game.process will do things such as update display, detect motion, play music, etc; it returns whether or not game is over
   }
   
+  //Serial.println(" .");
+  float leftover = LOOP - (millis() - primary_timer);
+  if (leftover < 0) {
+    Serial.printf("[Warning]: Loop over by %0.f ms!\n", -leftover);
+  } else {
+    delay(leftover);
+  }
+  primary_timer = millis();
 }

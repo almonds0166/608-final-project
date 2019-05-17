04b2b4859a8df218f2b45e703831e34492440427
Qi Qi <qqi@mit.edu>
2019-05-16 20:19:29 -0400

    first part of edits to bloody purity

--- server_side/charts/chart making tools/bloody_chart.py
+++ server_side/charts/chart making tools/bloody_chart.py
@@ -31,31 +31,36 @@ chart[5] = "l0d r0d r2r l2.5l r3l l3.5r r4d l6d l8u r8u r10r l10.5l r11d l11.5d 
 beat_offsets[5] = 168
 
 # syncopated
-chart[6] = "l0d r2r l3.5u l4d r5l r7u r8d l10l r11.5u r12d l13u l13.5l l14.5u l15r r16d l18l r19.5u r20d l21r l21.5l l23u l23.5d r24d r26r l28d r29u l30.5l r30.5r l31l r31r"
+chart[6] = "l1d r1.5d l2.5l r3u l4u r4.5r l5.5d r6u l7l r7.5r l8.5u r9d l10d r10.5l l11.5r r12u l13u r13.5u l14.5d r15d"
+chart[7] = "l1d r1d l1.5d r1.5d l2.5l r2.5u l3l r3u l4u r4r l4.5u r4.5r l5.5d r5.5u l6d r6u l7l r7r l7.5l r7.5r l8.5u r8.5d l9u r9d l10d r10l l10.5d r10.5l l11.5r r11.5u l12r r12u l13u r13u l13.5u r13.5u l14.5d r14.5d l15d r15d"
 beat_offsets[6] = 200
+beat_offsets[7] = 216
 
 # sixteenths
-chart[7] = "l0d r1d l2d r3d l4l r5r l6l r7r l8d r9d l10d r11d l12l r12r l16d r16d l18d r18d l20l r20r l22l r22r l24d r24u l26u r26d l28d r28d"
-beat_offsets[7] = 232
+chart[8] = "l0d r1d l2d r3d l4l r5r l6l r7r l8d r9d l10d r11d l12l r12r l16d r16d l18d r18d l20l r20r l22l r22r l24d r24u l26u r26d l28d r28d"
+beat_offsets[8] = 232
 
 # don't remember
-chart[8] = "r0.5d r1u l1.5u r2d l4d r5r l5.5r r6l l6.5l r7d d7.5d l8d r8d l10l r10r l10.5u r10.5u l11d r11d l11.5u r11.5u l12r r12r l16.5d l17u r17.5u l18d r18d l19l r19r l20d l21d l22d l23d l24d r24d l25d r25d l26d r26d l27d r27d l28l r28r l28.666666l r28.666666r l29.333333l r29.333333r l30d r30d"
-beat_offsets[8] = 268
+chart[9] = "r0.5d r1u l1.5u r2d l4d r5r l5.5r r6l l6.5l r7d d7.5d l8d r8d l10l r10r l10.5u r10.5u l11d r11d l11.5u r11.5u l12r r12r l16.5d l17u r17.5u l18d r18d l19l r19r l20d l21d l22d l23d l24d r24d l25d r25d l26d r26d l27d r27d l28l r28r l28.666666l r28.666666r l29.333333l r29.333333r l30d r30d"
+beat_offsets[9] = 268
 
 # syncopated part 2
-chart[9] = "l0.5u l1d r2d r2.5u l3.5u l4l r5d l5.5d l8r r8l r10d r10.5u l11.5u l12d r12d l12.666666l r12.666666r l13.333333u r13.333333u l14d r14d l14.666666r r14.666666l l15.333333l r15.333333r l16.5d r16.5u l17u r17d l18r r18r l19.5u r19.5d l20d r20u l21l r21l l24d r24d l26u r26u l28d r28.5d l29d r29.5d l30d r30.5d l31d r31.5d"
-beat_offsets[9] = 300
+# not yet edited
+chart[10] = "l0.5u l1d r2d r2.5u l3.5u l4l r5d l5.5d l8r r8l r10d r10.5u l11.5u l12d r12d l12.666666l r12.666666r l13.333333u r13.333333u l14d r14d l14.666666r r14.666666l l15.333333l r15.333333r l16.5d r16.5u l17u r17d l18r r18r l19.5u r19.5d l20d r20u l21l r21l l24d r24d l26u r26u l28d r28.5d l29d r29.5d l30d r30.5d l31d r31.5d"
+beat_offsets[10] = 300
 
 # fast ending
-chart[10] = "l0l l1l l2l l3l l4d l5d l6d r6.5d l7d r7.5d r8r r9r r10r r11r r12l r13l r14d l14.5d r15d l15.5d l16l r16r l20d r20d l24u r24u l28d r28d l32u r32u l32.5d r32.5d"
-beat_offsets[10] = 332
+chart[11] = "l0l l1l l2l l3l l4d l5d l6d r6.5d l7d r7.5d r8r r9r r10r r11r r12l r13l r14d l14.5d r15d l15.5d l16l r16r l20d r20d l24u r24u l28d r28d l32u r32u l32.5d r32.5d"
+beat_offsets[11] = 332
 
-offset = 1518
+offset = 1503
 bpm = 155
+duration = 147000
 num_notes = 0
 
 print("offset " + str(offset))
 print("bpm " + str(bpm))
+print("duration " + str(duration))
 print()
 
 def pretty_print(chart, offset_beat):
